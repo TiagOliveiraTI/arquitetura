612353521542a5621c0a6d00186ff333d6131843
tiagoliveirati <dev.tiago.oliveira@gmail.com>
2022-06-03 17:52:57 -0300

    test: ensure CPF is represented as string

--- tests/CpfTest.php
+++ tests/CpfTest.php
@@ -15,4 +15,10 @@ class CpfTest extends TestCase
         $this->expectException(InvalidArgumentException::class);
         new Cpf('12345678910');
     }
+
+    public function testCpfShouldBeRepresentedAsString()
+    {
+        $cpf = new Cpf('123.456.789-10');
+        $this->assertSame('123.456.789-10', (string) $cpf);
+    }
 }
