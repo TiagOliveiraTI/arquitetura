6c69fb65b67f6a195644dde4d352bfaf27b1c34d
tiagoliveirati <dev.tiago.oliveira@gmail.com>
2022-06-03 19:25:58 -0300

    feat: ensure Phone returns a string

--- src/Phone.php
+++ src/Phone.php
@@ -6,18 +6,19 @@ namespace OTaoDev\Arquitetura;
 
 use InvalidArgumentException;
 
-class Phone
+class Phone implements \Stringable
 {
     public function __construct(private string $ddd, private string $number)
     {
-        $this->dddValidate($ddd);
+        $this->setDdd($ddd);
+        $this->setNumber($this->number);
     }
 
-    private function dddValidate($ddd): Phone
+    private function setDdd($ddd): void
     {
         $options = [
             'options' => [
-                'regexp' => '/\d{3}/'
+                'regexp' => '/\d{2}/'
             ]
         ];
 
@@ -25,6 +26,20 @@ class Phone
             throw new InvalidArgumentException('DDD is in invalid format');
         }
 
-        return $this;
+        $this->ddd = $ddd;
+    }
+
+    private function setNumber($number): void
+    {
+        if (preg_match('/\d{8,9}/', $number) !== 1) {
+            throw new InvalidArgumentException('number is in invalid format');
+        }
+
+        $this->number = $number;
+    }
+
+    public function __toString(): string
+    {
+        return "({$this->ddd}) {$this->number}";
     }
 }
