2a565914f8d4fc3bc7f8857c9c437b6e3faf5e6c
tiagoliveirati <dev.tiago.oliveira@gmail.com>
2022-06-03 19:26:20 -0300

    test: ensure Phone returns a string

--- tests/PhoneTest.php
+++ tests/PhoneTest.php
@@ -13,6 +13,20 @@ class PhoneTest extends TestCase
     public function testDddWithAnInvalidFormatShouldThrows()
     {
         $this->expectException(InvalidArgumentException::class);
+        $this->expectDeprecationMessage('DDD is in invalid format');
         new Phone('ddd','invalid_phone_number');
+    }
+
+    public function testPhoneNumberWithAnInvalidFormatShouldThrows()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectDeprecationMessage('number is in invalid format');
+        new Phone('11','invalid_phone_number');
+    }
+
+    public function testNumberShouldBeRepresentedAsString()
+    {
+        $ddd = new Phone('11','987654321');
+        $this->assertSame('(11) 987654321', (string) $ddd);
     }
 }
