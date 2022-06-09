5c7f39e9415c98ec9c8c0f88ce0411a9f479cc1c
tiagoliveirati <dev.tiago.oliveira@gmail.com>
2022-06-09 17:37:16 -0300

    feat: ensure Student class creates a phone number using named constructor method

---
+++ src/Student.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OTaoDev\Arquitetura;
+
+class Student
+{
+    private Cpf $cpf;
+    private string $name;
+    private Email $email;
+    private array $phones;
+
+    public function __construct(Cpf $cpf, string $name, Email $email)
+    {
+        $this->cpf = $cpf;
+        $this->name = $name;
+        $this->email = $email;
+    }
+
+    public static function withCpfNameAndEmail(string $cpf, string $name, string $email)
+    {
+        return new Student(new Cpf($cpf), $name, new Email($email));
+    }
+
+    public function addPhoneNumber(string $ddd, string $number)
+    {
+        $this->phones[] = new Phone($ddd, $number);
+        return $this;
+    }
+}
